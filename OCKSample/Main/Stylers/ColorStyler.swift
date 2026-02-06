2f4d49929b25dd80c404c1352401f06eae63e366
AlarikDamrow <adamrow@usc.edu>
2026-02-06 00:35:45 +0000

    Setting up GitHub Classroom Feedback

--- OCKSample/Main/Stylers/ColorStyler.swift
+++ OCKSample/Main/Stylers/ColorStyler.swift
@@ -13,10 +13,21 @@ import UIKit
 struct ColorStyler: OCKColorStyler {
     #if os(iOS) || os(visionOS)
     var label: UIColor {
-        FontColorKey.defaultValue
+        //FontColorKey.defaultValue
+        UIColor.systemBlue
+    }
+    var secondaryLabel: UIColor {
+        UIColor.systemBlue.withAlphaComponent(0.75)
     }
     var tertiaryLabel: UIColor {
-		UIColor(Color.accentColor)
+		//UIColor(Color.accentColor)
+        UIColor.systemBlue.withAlphaComponent(0.5)
+    }
+    var systemGroupedBackground: UIColor {
+        UIColor.white
+    }
+    var separator: UIColor {
+        UIColor.systemBlue.withAlphaComponent(0.2)
     }
     #endif
 }
